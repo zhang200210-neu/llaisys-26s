081c364ef7827a4c542db28ef1e6351d601e6a14
zhang200210-neu <wj2my4ttgg@privaterelay.appleid.com>
2026-08-06 01:24:10 +0800

    Update op.cpp

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,16 @@
 #include "op.hpp"
 
-namespace llaisys::ops {
-void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+namespace llaisys::ops::cpu {
+void argmax(std::byte *max_idx, std::byte *max_val, const std::byte *vals, llaisysDataType_t type, size_t numel) {
+	switch (type) {
+	case LLAISYS_DTYPE_F32:
+		return argmax_impl<float>(max_idx, max_val, vals, numel);
+	case LLAISYS_DTYPE_BF16:
+		return argmax_impl<llaisys::bf16_t>(max_idx, max_val, vals, numel);
+	case LLAISYS_DTYPE_F16:
+		return argmax_impl<llaisys::fp16_t>(max_idx, max_val, vals, numel);
+	default:
+		EXCEPTION_UNSUPPORTED_DATATYPE(type);
+	}
 }
-} // namespace llaisys::ops
+} // namespace llaisys::ops::cpu
